65d1916948fd4ed60a698f1a0c7c6badfd587b03
S4M37 <sametoussema@mail.com>
2017-07-11 17:11:24 +0100

    refactoring api routing

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,27 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//User API
+Route::group(['prefix' => 'users'], function () {
+    Route::get('', 'UserController@index');
+    Route::get('{user_id}', 'UserController@getUserById');
+    Route::put('{user_id}', 'UserController@update');
+    Route::delete('{user_id}', 'UserController@delete');
+    Route::post('register', 'UserController@register');
+    Route::get('{user_id}/validate/{validation_code}', 'UserController@validateUser');
+    Route::get('{user_id}/sendConfirmationEmail', 'UserController@resendConfirmationMail');
+});
 
+//Geo API
+Route::group(['prefix' => 'geo'], function () {
+    Route::group(['prefix' => 'countries'], function () {
+        Route::get('', 'GeoController@getAllCountries');
+        Route::get('{country_id}/cities', 'GeoController@getCitiesByCountry');
+    });
+    Route::group(['prefix' => 'cities'], function () {
+        Route::get('', 'GeoController@getAllCities');
+    });
+});
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -3,27 +3,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::group(['prefix' => 'api'], function () {
-    //User API
-    Route::group(['prefix' => 'users'], function () {
-        Route::get('', 'UserController@index');
-        Route::get('{user_id}', 'UserController@getUserById');
-        Route::put('{user_id}', 'UserController@update');
-        Route::delete('{user_id}', 'UserController@delete');
-        Route::post('register', 'UserController@register');
-        Route::get('{user_id}/validate/{validation_code}', 'UserController@validateUser');
-        Route::get('{user_id}/sendConfirmationEmail', 'UserController@resendConfirmationMail');
-    });
-
-    //Geo API
-    Route::group(['prefix' => 'geo'], function () {
-        Route::group(['prefix' => 'countries'], function () {
-            Route::get('', 'GeoController@getAllCountries');
-            Route::get('{country_id}/cities', 'GeoController@getCitiesByCountry');
-        });
-        Route::group(['prefix' => 'cities'], function () {
-            Route::get('', 'GeoController@getAllCities');
-        });
-    });
-});
