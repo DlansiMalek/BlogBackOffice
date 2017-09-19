4c6b2db32bc8d6982d4da4b1a994f99711162797
abbesyassine <abbesyassine4@gmail.com>
2017-09-19 23:40:33 +0100

    fixing final badge

--- resources/views/pdf/badge.blade.php
+++ resources/views/pdf/badge.blade.php
@@ -11,7 +11,6 @@
 
         .images h1 {
             position: absolute;
-            width: 100%;
             text-align: center;
             bottom: 450px;
             left: 0;
@@ -20,7 +19,8 @@
         .images .qrCode {
             position: absolute;
             width: 150px;
-            bottom: 0;
+            margin-top: 670px;
+            top: 0;
             left: 0;
 
         }
@@ -28,7 +28,7 @@
 </head>
 <body>
 <div class="images">
-    <img style="height: 100%" src="{{public_path().'/badge/badge.jpg'}}"/>
+    <img style="width: 100% ;height: 100%" src="{{public_path().'/badge/badge.jpg'}}"/>
     <h1>{{$name}}</h1>
     <img class="qrCode" src="{{public_path().'/QRcode/qrcode.png'}}">
 </div>
