92068a0fab119c44a99febefbd87597caf071dcd
abbesyassine <abbesyassine4@gmail.com>
2018-03-06 00:36:15 +0100

    fixing badge

--- resources/views/pdf/badge.blade.php
+++ resources/views/pdf/badge.blade.php
@@ -2,30 +2,24 @@
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-
     <style>
-
         .badge {
             position: relative;
         }
-
         .badge-img {
             width: 100%;
             position: absolute;
             left: 0;
             top: 0;
         }
-
         .img-qr_code {
             width: 90px;
             height: 90px;
         }
-
         .badge-qrCode {
             position: absolute;
-            right: 105px;
-            top: 205px;
-            margin-top: 5px;
+            right: 60px;
+            top: 180px;
         }
 
     </style>
@@ -36,8 +30,8 @@
         <td>
             <div class="badge">
                 <img class="badge-img" src="{{public_path().'/badge/ticket.jpg'}}"/>
-                <div class="badge-qrCode img-qr_code">
-                    <img src="{{public_path().'/QRcode/qrcode.png'}}">
+                <div class="badge-qrCode">
+                    <img class="img-qr_code" src="{{public_path().'/QRcode/qrcode.png'}}">
                 </div>
             </div>
         </td>
