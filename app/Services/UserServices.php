baa6b8c490b217e7f3e0450d59d68bfeb775d8a4
Fouad Wahabi <fouad.wahabi@gmail.com>
2018-05-23 08:53:36 -0500

    Fix congressId column

--- app/Services/UserServices.php
+++ app/Services/UserServices.php
@@ -46,7 +46,7 @@ class UserServices
         $qrcode = Utils::generateCode($newUser->id_User);
         $newUser->qr_code = $qrcode;
 
-        $newUser->congressId = $request->input("congressId");
+        $newUser->congress_id = $request->input("congressId");
 
         $user = $newUser->save();
 
