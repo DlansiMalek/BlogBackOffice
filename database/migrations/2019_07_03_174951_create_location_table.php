1e56dcfe0495d6ddfe286832bff7fdb8a2dc15b8
borchaniz <borchaniz96@gmail.com>
2019-07-07 16:45:02 +0100

    small fix

---
+++ database/migrations/2019_07_03_174951_create_location_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Location', function (Blueprint $table) {
+            $table->increments('location_id');
+
+            $table->string('label');
+            $table->string('longitude');
+            $table->string('latitude');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('location');
+    }
+}
