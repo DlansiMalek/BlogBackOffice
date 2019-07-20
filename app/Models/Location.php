74b7e177a513827377e5b57a42018c4f6d463c96
Abbes Yassine <abbesyassine4@gmail.com>
2019-07-20 16:53:05 +0100

    do some changes

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -9,15 +9,18 @@ class Location extends Model
 
     protected $table = 'Location';
     protected $primaryKey = 'location_id';
-    protected $fillable = ['lng', 'lat', 'address'];
+    protected $fillable = ['lng', 'lat', 'address', 'congress_id'];
 
     protected $dates = ['created_at', 'updated_at'];
     public $timestamps = true;
 
-    public function congress(){
-        return $this->hasMany('App\Models\Congress','congress_id','congress_id');
+    public function congress()
+    {
+        return $this->hasOne('App\Models\Congress', 'congress_id', 'congress_id');
     }
-    public function city(){
-        return $this->belongsTo('App\Models\City','city_id','city_id');
+
+    public function city()
+    {
+        return $this->belongsTo('App\Models\City', 'city_id', 'city_id');
     }
 }
--- app/Services/GeoServices.php
+++ app/Services/GeoServices.php
@@ -14,25 +14,31 @@ class GeoServices
         return Country::All();
     }
 
-    public function getCityByNameAndCountryCode($name,$countryCode) {
-        return City::where('name','=',$name)
-            ->where('country_code','=',$countryCode)
-            ->first();
-    }
-    public function getCongressLocationByCongressId($congressId) {
-        return Location::leftJoin('congress', 'Location.location_id', '=', 'congress.location_id')
-            ->where('congress_id','=',$congressId)
+    public function getCityByNameAndCountryCode($name, $countryCode)
+    {
+        return City::where('name', '=', $name)
+            ->where('country_code', '=', $countryCode)
             ->first();
     }
 
-    public function getLocationById($locationId) {
-        return Location::where('location_id','=',$locationId)
+    public function getCongressLocationByCongressId($congressId)
+    {
+        return Location::where('congress_id', '=', $congressId)
             ->first();
     }
-    public function getCountryByCode($shortname) {
-        return Country::where('code','=',$shortname)
+
+    public function getLocationById($locationId)
+    {
+        return Location::where('location_id', '=', $locationId)
             ->first();
     }
+
+    public function getCountryByCode($shortname)
+    {
+        return Country::where('code', '=', $shortname)
+            ->first();
+    }
+
     public function getCountryById($country_id)
     {
         return Country::find($country_id);
