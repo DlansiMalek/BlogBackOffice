9e8e254c165653480090d4ede6b26e81af27cbf5
osamet_a2lean <oussema.samet@adelean.com>
2019-11-29 11:37:23 +0100

    Fix deploy-dev

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -9,8 +9,8 @@ docker-compose -f docker-compose-dev.yml kill
 docker-compose -f docker-compose-dev.yml up -d --build
 
 
-docker exec  eventizer-api-app chown -R www-data.www-data .
-docker exec  eventizer-api-app chmod -R 755 .
-docker exec  eventizer-api-app chmod -R 777 ./app
-docker exec  eventizer-api-app chmod -R 777 ./vendor
-docker exec  eventizer-api-app php artisan key:generate
+docker exec  eventizer-api-dev-app chown -R www-data.www-data .
+docker exec  eventizer-api-dev-app chmod -R 755 .
+docker exec  eventizer-api-dev-app chmod -R 777 ./app
+docker exec  eventizer-api-dev-app chmod -R 777 ./vendor
+docker exec  eventizer-api-dev-app php artisan key:generate
