7605f1fb1bd03ecae84b7f45fe511f698c355329
mohamedwaelbenismail <mohamedwaelbenismail@gmail.com>
2020-05-04 15:05:03 +0100

    security-room1.0

--- app/Services/UserServices.php
+++ app/Services/UserServices.php
@@ -1060,7 +1060,7 @@ class UserServices
     public function checkUserRights($user)
     {
         
-        if ($user && $user->user_access[0]['jetsi_token']){
+        if ($user && sizeof($user->accesses) > 0 && $user->user_access[0]['jetsi_token']){
             return 1;
         }
         if ($user && sizeof($user->user_congresses) > 0 && $user->payments[0]['isPaid'] == 1 && sizeof($user->accesses) > 0 ){
