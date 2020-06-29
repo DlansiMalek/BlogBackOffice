4c5cd4eb7cf08a2512341ed2ef7e35183e8c28a5
omar-chaabouni <55398565+omar-chaabouni@users.noreply.github.com>
2020-06-29 13:25:05 +0100

    on delete correction

---
+++ database/migrations/2020_06_29_122028_add_on_delete_to_access_pack_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOnDeleteToAccessPackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Access_Pack', function (Blueprint $table) {
+            $table->dropForeign('pack_id');
+            $table->foreign('pack_id')->references('pack_id')->on('Pack')->onDelete('cascade');
+            $table->dropForeign('access_id');
+            $table->foreign('access_id')->references('access_id')->on('Access')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Access_Pack', function (Blueprint $table) {
+            $table->dropForeign(['pack_id']);
+            $table->dropForeign(['access_id']);
+        });
+    }
+}
