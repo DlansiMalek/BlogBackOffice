3bfa67c0489d5da0fbc939bcac102e0acc6a6c21
omar-chaabouni <55398565+omar-chaabouni@users.noreply.github.com>
2020-07-01 12:21:18 +0100

    upgrade to laravel 7 comm 1

--- config/app.php
+++ config/app.php
@@ -176,7 +176,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Barryvdh\Cors\ServiceProvider::class,
         //Barryvdh\DomPDF\ServiceProvider::class,
         SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
