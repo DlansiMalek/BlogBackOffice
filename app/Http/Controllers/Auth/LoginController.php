e126e7430b999a1c084798cd299b0f77e1b41f5f
nour <nour.abdennebi@esen.tn>
2020-09-27 17:49:24 +0100

    fixing

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -83,6 +83,10 @@ class LoginController extends Controller
             return response()->json(['error' => 'invalid credentials'], 401);
         }
 
+        if ($user->email_verified == 0) {
+            return response()->json(['error' => 'email not verified'], 405);
+        }
+
         return response()->json(['user' => $user, 'token' => $token], 200);
     }
 
