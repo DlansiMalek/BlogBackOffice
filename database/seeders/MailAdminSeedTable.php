69c984d263eadfa8b7125637dc2250ef90be9a2b
nour <nour.abdennebi@esen.tn>
2021-03-09 16:05:08 +0100

    fixing

--- database/seeders/MailAdminSeedTable.php
+++ database/seeders/MailAdminSeedTable.php
@@ -45,14 +45,13 @@ class MailAdminSeedTable extends Seeder
             'mail_type_admin_id' => 7
         ]);
         DB::table('Mail_Admin')->insert([
-            'object' => "Acceptation du votre demande Landing Page",
-            'template' => '<p>votre demande de landing page est acceptée avec succès </p><ul><li>VotreDns: {{$linkBackOffice}}</li></ul><p>Accéedez à votre Landing page via :<a> </a></p>',
+            'object' => "Acceptation du votre demande de création de Landing Page",
+            'template' => '<p>votre demande de création de landing page est acceptée</p><ul></ul><p>Accéedez à votre Landing page via : <a href="{{$linkBackOffice}}">Lien</a></p>',
             'mail_type_admin_id' => 8
         ]);
         DB::table('Mail_Admin')->insert([
-            'object' => "votre demande Landing Page est Refusée",
-            'template' => '<p>votre demande de landing page est Refusée 
-            , vous pouvez réessayer une autre fois </p>',
+            'object' => "Refus de votre demande de création de Landing Page",
+            'template' => '<p>votre demande de création de landing page est refusée, vous pouvez réessayer une autre fois </p>',
             'mail_type_admin_id' => 9
         ]);
     }
