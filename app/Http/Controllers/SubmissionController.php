0c029d2b10ae1e9beba863cb842b9e3a0792a530
ABBES Yassine <abbes.yassine@outlook.fr>
2021-07-01 12:26:02 +0200

    fixing make masse elligible

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -996,33 +996,15 @@ class SubmissionController extends Controller
         if (!$congress = $this->congressServices->getCongressById($congressId)) {
             return response(['error' => "congress not found"], 404);
         }
-        foreach ($subs as $sub) {
-            if (!$submission = $this->submissionServices->getSubmissionByIdWithRelation(
-                ['authors', 'user'],
-                $sub
-            )) {
-                return response(['error' => "submission not found"], 404);
-            }
-            if ($submission->congress_id != $congressId) {
-                return response(['error' => "an error has occurred"], 400);
-            }
-            if ($submission->status != 1) {
-                return response(['error' => "submission" + $eligibility + " not selected"], 400);
-            }
-            try {
-                $admin = $this->adminServices->retrieveAdminFromToken();
-                if (!$adminCongress = $this->congressServices->getAdminByCongressId($congressId, $admin)) {
-                    return response()->json(['error' => 'bad request'], 400);
-                }
+        foreach ($subs as $submissionId) {
+            $submission = $this->submissionServices->getSubmissionByIdWithRelation([],$submissionId);
+            if ($submission->status === 1) {
                 if ($eligibility == "true") {
                     $response = $this->submissionServices->makeSubmissionEligible($submission);
                 }
                 if ($eligibility == "false") {
                     $response = $this->submissionServices->makeSubmissionNotEligible($submission);
                 }
-            } catch (Exception $e) {
-                Log::info($e->getMessage());
-                return response()->json(['response' => $e->getMessage()], 400);
             }
         }
         return response()->json(['Changes done successfully'], 200);
