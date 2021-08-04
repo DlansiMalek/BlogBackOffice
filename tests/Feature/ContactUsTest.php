9bc85a6db6ff6870f3cd412f2e70de3e4d2e35c3
jaziri.hytham <jaziri.hytham@gmail.com>
2021-08-04 11:14:14 +0100

    fix retour

---
+++ tests/Feature/ContactUsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\ContactUs;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Log;
+
+class ContactUsTest extends TestCase
+{
+
+  public function testSendContactUs()
+  {
+    $contact =  $this->getFakeContact();
+    $response =  $this->post('api/contact-us/send', $contact)
+      ->assertStatus(200);
+  }
+
+  private function getFakeContact()
+  {
+    return [
+      'email' => $this->faker->word,
+      'user_name' =>  $this->faker->word,
+      'subject'   =>  $this->faker->word,
+      'message'  =>  $this->faker->word,
+      'mobile' =>  $this->faker->word,
+    ];
+  }
+}
