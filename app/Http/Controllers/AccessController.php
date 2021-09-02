29e3375ffd8bf6a972eb909c393615d98ff301a6
lasoued.iheb@gmail.com <Iheb.lassoued@esprit.tn>
2021-09-02 15:52:19 +0100

    fixing retour

--- app/Http/Controllers/AccessController.php
+++ app/Http/Controllers/AccessController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use App\Models\UserAccess;
 use App\Services\AccessServices;
 use App\Services\AdminServices;
 use App\Services\CongressServices;
@@ -13,9 +11,6 @@ use App\Services\RoomServices;
 use App\Services\UserServices;
 use App\Services\Utils;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Str;
-
 
 class AccessController extends Controller
 {
@@ -28,15 +23,13 @@ class AccessController extends Controller
     protected $roomServices;
     protected $notificationServices;
 
-
-    function __construct(AccessServices $accessServices,
-                         AdminServices $adminServices,
-                         UserServices $userServices,
-                         CongressServices $congressServices,
-                         ResourcesServices $resourcesServices,
-                         RoomServices $roomServices,
-                         NotificationServices $notificationServices)
-    {
+    public function __construct(AccessServices $accessServices,
+        AdminServices $adminServices,
+        UserServices $userServices,
+        CongressServices $congressServices,
+        ResourcesServices $resourcesServices,
+        RoomServices $roomServices,
+        NotificationServices $notificationServices) {
         $this->accessServices = $accessServices;
         $this->adminServices = $adminServices;
         $this->userServices = $userServices;
@@ -46,8 +39,7 @@ class AccessController extends Controller
         $this->notificationServices = $notificationServices;
     }
 
-
-    function startAccessById(Request $request)
+    public function startAccessById(Request $request)
     {
         $accessId = $request->input('access_id');
 
@@ -73,17 +65,20 @@ class AccessController extends Controller
 
     }
 
-
     public function addAccess(Request $request, $congress_id)
     {
-        if (!$request->has(['name', 'start_date', 'end_date', 'access_type_id']))
+        if (!$request->has(['name', 'start_date', 'end_date', 'access_type_id'])) {
             return response()->json(['response' => 'invalid request',
                 'required fields' => ['name', 'start_date', 'end_date', 'access_type_id']], 400);
-        if (!$congress = $this->congressServices->getCongressById($congress_id))
-            return response()->json(['response' => 'congress not found'], 404);
+        }
 
-        if (!$admin = $this->adminServices->retrieveAdminFromToken())
+        if (!$congress = $this->congressServices->getCongressById($congress_id)) {
+            return response()->json(['response' => 'congress not found'], 404);
+        }
+
+        if (!$admin = $this->adminServices->retrieveAdminFromToken()) {
             return response()->json(['response' => 'admin not found'], 404);
+        }
 
         $access = $this->accessServices->addAccess($congress_id, $request);
         $token_jitsi = $this->roomServices->createToken($admin->email, 'eventizer_room_' . $congress_id . $access->access_id, true, $admin->name);
@@ -140,8 +135,9 @@ class AccessController extends Controller
 
     public function editAccess(Request $request, $access_id)
     {
-        if (!$access = $this->accessServices->getAccessById($access_id))
+        if (!$access = $this->accessServices->getAccessById($access_id)) {
             return response()->json(['message' => 'access not found'], 404);
+        }
 
         $access = $this->accessServices->editAccess($access, $request);
 
@@ -151,29 +147,35 @@ class AccessController extends Controller
 
         if ($request->has('chair_ids') && count($request->input('chair_ids'))) {
             $this->accessServices->editChairs($access_id, $request->input('chair_ids'));
-        } else $this->accessServices->removeAllChairs($access_id);
+        } else {
+            $this->accessServices->removeAllChairs($access_id);
+        }
 
         if ($request->has('speaker_ids') && count($request->input('speaker_ids'))) {
             $this->accessServices->editSpeakers($access_id, $request->input('speaker_ids'));
-        } else $this->accessServices->removeAllSpeakers($access_id);
+        } else {
+            $this->accessServices->removeAllSpeakers($access_id);
+        }
 
         if ($request->has('resource_ids') && count($request->input('resource_ids'))) {
             $this->resourcesServices->editAccessResources($access_id, $request->input('resource_ids'));
-        } else $this->resourcesServices->removeAllResources($access_id);
-
+        } else {
+            $this->resourcesServices->removeAllResources($access_id);
+        }
 
         if ($request->has('sub_accesses') && count($request->input('sub_accesses'))) {
             $this->accessServices->editSubAccesses($access, $request->input('sub_accesses'));
-        } else $this->accessServices->deleteAllSubAccesses($access_id);
+        } else {
+            $this->accessServices->deleteAllSubAccesses($access_id);
+        }
 
         $this->notificationServices->sendNotificationToCongress('Changement du programme: ' . $access->name
             , $access->congress_id);
 
-    
+        $this->congressServices->deleteAllAllowedAccessByCongressId($access->congress_id, $access->access_id);
+
         if ($request->has('privileges') && count($request->input('privileges'))) {
             $this->congressServices->addAllAllowedAccessByCongressId($request->input('privileges'), $access->congress_id, $access->access_id);
-        } else {
-            $this->congressServices->deleteAllAllowedAccessByCongressId($access->congress_id, $access->access_id);
         }
         return $this->accessServices->getAccessById($access->access_id);
     }
@@ -210,16 +212,16 @@ class AccessController extends Controller
         return response()->json(['message' => 'not found'], 404);
     }
 
-    function updateTokensJitsi($congressId)
+    public function updateTokensJitsi($congressId)
     {
         $accesses = $this->accessServices->getAccesssByCongressId($congressId);
 
         foreach ($accesses as $access) {
-                $jitsi_moderator = $this->roomServices->createToken('moderator@eventizer.io', 'eventizer_room_' . $congressId . $access->access_id, true, "Moderator");
-                $jitsi_participant = $this->roomServices->createToken('participant@eventizer.io', 'eventizer_room_' . $congressId . $access->access_id, false, "Participant");
-                $access->token_jitsi_moderator = $jitsi_moderator;
-                $access->token_jitsi_participant = $jitsi_participant;
-                $access->update();
+            $jitsi_moderator = $this->roomServices->createToken('moderator@eventizer.io', 'eventizer_room_' . $congressId . $access->access_id, true, "Moderator");
+            $jitsi_participant = $this->roomServices->createToken('participant@eventizer.io', 'eventizer_room_' . $congressId . $access->access_id, false, "Participant");
+            $access->token_jitsi_moderator = $jitsi_moderator;
+            $access->token_jitsi_participant = $jitsi_participant;
+            $access->update();
 
         }
     }
@@ -229,19 +231,19 @@ class AccessController extends Controller
         if (!$this->congressServices->getCongressById($congress_id)) {
             return response()->json(['response' => 'Congress not found', 404]);
         }
-        
+
         $all = $request->query('all', false);
         $status = $request->query('status', 1);
         $access_id = $request->query('accessId', null);
 
-        if ($all=='true') {
+        if ($all == 'true') {
             $this->accessServices->editAllAccessesStatus($congress_id, $status);
         } else {
             $this->accessServices->editAccessStatus($access_id, $status);
         }
         return response()->json($this->accessServices->getByCongressId($congress_id));
     }
-    
+
     public function getScoresByCongressId($congress_id, Request $request)
     {
         if (!$this->congressServices->getById($congress_id)) {
@@ -250,9 +252,8 @@ class AccessController extends Controller
         $access_id = $request->query('access_id');
         if ($access_id && $access_id != 'null') {
             $access = $this->accessServices->getAccessById($access_id);
-            if (!$access || $access->access_type_id !=4)
-            {
-            return response()->json(['response' => 'bad request'], 400);
+            if (!$access || $access->access_type_id != 4) {
+                return response()->json(['response' => 'bad request'], 400);
             }
             $access_game = $this->accessServices->getScoresByAccess($congress_id, $access_id);
         } else {
@@ -268,7 +269,7 @@ class AccessController extends Controller
             return response()->json(['response' => 'congress not found'], 404);
         }
 
-        if(!$request->has(['user_id', 'score'])){
+        if (!$request->has(['user_id', 'score'])) {
             return response()->json(['response' => 'bad request (required user_id, score)']);
         }
 
@@ -277,8 +278,7 @@ class AccessController extends Controller
         }
         $name = $request->query('name');
         $access = $this->accessServices->getAccessByName($name);
-        if (!$access || $access->access_type_id !=4)
-        {
+        if (!$access || $access->access_type_id != 4) {
             return response()->json(['response' => 'Access not found or not type Game'], 400);
         }
         $accessGame = $this->accessServices->saveScoreGame($access->access_id, $request);
@@ -291,11 +291,10 @@ class AccessController extends Controller
             return response()->json(['response' => 'congress not found'], 404);
         }
         $name = $request->query('name');
-        if ($name && !is_null($name) && $name !='null') {
+        if ($name && !is_null($name) && $name != 'null') {
             $access = $this->accessServices->getAccessByName($name);
-            if (!$access || $access->access_type_id !=4)
-            {
-            return response()->json(['response' => 'bad request'], 400);
+            if (!$access || $access->access_type_id != 4) {
+                return response()->json(['response' => 'bad request'], 400);
             }
             $access_game = $this->accessServices->getScoresByAccess($congress_id, $access->access_id, true);
         } else {
@@ -308,15 +307,14 @@ class AccessController extends Controller
     public function resetScore($access_id)
     {
         $access = $this->accessServices->getAccessById($access_id);
-        if (!$access || $access->access_type_id !=4)
-        {
-             return response()->json(['response' => 'bad request'], 400);
+        if (!$access || $access->access_type_id != 4) {
+            return response()->json(['response' => 'bad request'], 400);
         }
         $this->accessServices->resetScore($access_id);
         return response()->json('deleted successfully', 200);
     }
 
-    public function uploadExcelAccess($congress_id, Request $request) 
+    public function uploadExcelAccess($congress_id, Request $request)
     {
         if (!$this->congressServices->getById($congress_id)) {
             return response()->json(['response' => 'congress not found'], 404);
@@ -329,28 +327,27 @@ class AccessController extends Controller
         $accesses = $request->input("data");
         $errors = '';
 
-        if (!$oldAccesses = $this->accessServices->getByCongressId($congress_id)) 
+        if (!$oldAccesses = $this->accessServices->getByCongressId($congress_id)) {
             $oldAccesses = [];
+        }
 
-
-        foreach($oldAccesses as $old) 
-        {
+        foreach ($oldAccesses as $old) {
             $found = false;
             foreach ($accesses as $access) {
                 if ($access['email']) {
                     $user = $this->userServices->getUserByEmail($access['email'], $congress_id);
                     if ($user && count($user->user_congresses) > 0 && ($user->user_congresses[0]->privilege_id == config('privilege.Moderateur') || $user->user_congresses[0]->privilege_id == config('privilege.Conferencier_Orateur'))) {
-                        $start_date = isset($access['start_date']) ? $access['start_date'] : null ;
-                        $end_date = isset($access['end_date']) ? $access['end_date'] : null ;
+                        $start_date = isset($access['start_date']) ? $access['start_date'] : null;
+                        $end_date = isset($access['end_date']) ? $access['end_date'] : null;
                         $name = Utils::setAccessName($start_date, $end_date, $user->first_name . ' ' . $user->last_name);
-                        if ($old->access_type_id == $access_type_id && $old->name == $name && $old->start_date == $start_date && $old->end_date == $end_date ) {
+                        if ($old->access_type_id == $access_type_id && $old->name == $name && $old->start_date == $start_date && $old->end_date == $end_date) {
                             $found = true;
                             break;
                         }
-                    } 
-                }  
+                    }
+                }
             }
-            if (!$found && ( count($old->speakers) > 0  || count($old->chairs) > 0 ) && count($old->packs) == 0) {
+            if (!$found && (count($old->speakers) > 0 || count($old->chairs) > 0) && count($old->packs) == 0) {
                 $this->accessServices->deleteAccess($old->access_id);
             }
         }
@@ -360,28 +357,28 @@ class AccessController extends Controller
             if ($access['email']) {
                 $user = $this->userServices->getUserByEmail($access['email'], $congress_id);
                 if ($user && count($user->user_congresses) > 0 && ($user->user_congresses[0]->privilege_id == config('privilege.Moderateur') || $user->user_congresses[0]->privilege_id == config('privilege.Conferencier_Orateur'))) {
-                    $start_date = isset($access['start_date']) ? $access['start_date'] : null ;
-                    $end_date = isset($access['end_date']) ? $access['end_date'] : null ;
+                    $start_date = isset($access['start_date']) ? $access['start_date'] : null;
+                    $end_date = isset($access['end_date']) ? $access['end_date'] : null;
                     $name = Utils::setAccessName($start_date, $end_date, $user->first_name . ' ' . $user->last_name);
-                foreach($oldAccesses as $old) {
-                    if ($old->access_type_id == $access_type_id && $old->name == $name && $old->start_date == $start_date && $old->end_date == $end_date) {
-                        $found = true;
-                        break;
+                    foreach ($oldAccesses as $old) {
+                        if ($old->access_type_id == $access_type_id && $old->name == $name && $old->start_date == $start_date && $old->end_date == $end_date) {
+                            $found = true;
+                            break;
+                        }
                     }
+                    if (!$found) {
+                        $newAccess = $this->accessServices->addAccessFromExcel($start_date, $end_date, $access_type_id, $congress_id, $name);
+                        $user->user_congresses[0]->privilege_id == config('privilege.Moderateur') ? $this->accessServices->addChair($newAccess->access_id, $user->user_id) : $this->accessServices->addSpeaker($newAccess->access_id, $user->user_id);
+                    }
+                } else {
+                    $errors = $errors . ' ' . $access['line'];
                 }
-                if (!$found) {
-                    $newAccess = $this->accessServices->addAccessFromExcel($start_date, $end_date, $access_type_id, $congress_id, $name );
-                    $user->user_congresses[0]->privilege_id == config('privilege.Moderateur') ? $this->accessServices->addChair($newAccess->access_id ,$user->user_id) : $this->accessServices->addSpeaker($newAccess->access_id ,$user->user_id);
-                }
-            } else {
-                $errors = $errors . ' ' . $access['line'];
             }
-          }
         }
         $allAccesses = $this->accessServices->getByCongressId($congress_id);
         return response()->json(['accesses' => $allAccesses, 'errors' => $errors], 200);
     }
-    
+
     public function getUserAccessesByCongressId($congress_id)
     {
         if (!$congress = $this->congressServices->getCongressById($congress_id)) {
@@ -396,7 +393,7 @@ class AccessController extends Controller
     }
     public function getAccessesByCongressIdPginantion($congressId, Request $request)
     {
-                
+
         $offset = $request->query('offset', 0);
         $perPage = $request->query('perPage', 10);
         $search = $request->query('search', '');
@@ -410,9 +407,9 @@ class AccessController extends Controller
             if (!$user) {
                 return response()->json(['response' => 'No user found'], 401);
             }
-        $accesses = $this->accessServices->getAccessesByCongressIdPginantion($congressId, $offset, $perPage, $search, $date, $startTime, $endTime, $isOnline,$myAccesses, $user->user_id);
+            $accesses = $this->accessServices->getAccessesByCongressIdPginantion($congressId, $offset, $perPage, $search, $date, $startTime, $endTime, $isOnline, $myAccesses, $user->user_id);
         } else {
-        $accesses = $this->accessServices->getAccessesByCongressIdPginantion($congressId, $offset, $perPage, $search, $date, $startTime, $endTime, $isOnline, $myAccesses, null);
+            $accesses = $this->accessServices->getAccessesByCongressIdPginantion($congressId, $offset, $perPage, $search, $date, $startTime, $endTime, $isOnline, $myAccesses, null);
         }
         return response()->json($accesses, 200);
     }
