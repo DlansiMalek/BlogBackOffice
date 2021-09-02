025ee882caa97aee54ba102bdab63120ddda5c88
nourabdennebi123 <nour.abdennebi@esen.tn>
2021-09-02 11:23:07 +0100

    fixing-seeders-again

--- database/seeders/MailTypeSeedTable.php
+++ database/seeders/MailTypeSeedTable.php
@@ -148,24 +148,5 @@ class MailTypeSeedTable extends Seeder
             'display_name' => "A réviser une soumission",
             'type' => "submission"
         ]);
-        DB::table('Mail_Type')->insert([
-            'mail_type_id' => 24,
-            'name' => 'request_meeting',
-            'display_name' => "Demande de réunion ",
-            'type' => "event"
-        ]);
-        DB::table('Mail_Type')->insert([
-            'mail_type_id' => 25,
-            'name' => 'accept_meeting',
-            'display_name' => "Acceptation de la demande de réunion",
-            'type' => "event"
-        ]);
-        DB::table('Mail_Type')->insert([
-            'mail_type_id' => 26,
-            'name' => 'decline_meeting',
-            'display_name' => "Refus de la demande de réunion",
-            'type' => "event"
-        ]);
-
     }
 }
