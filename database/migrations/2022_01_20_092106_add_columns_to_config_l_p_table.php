71e081c241e7ad4e2d2e6ce58add7edf608e1e75
Akkeri <akkeriamani@gmail.com>
2022-01-20 10:31:19 +0100

    add-migration-configLP

---
+++ database/migrations/2022_01_20_092106_add_columns_to_config_l_p_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToConfigLPTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Config_LP', function (Blueprint $table) {
+            $table->string('waiting_title')->nullable()->default(null);
+            $table->text('waiting_desription')->nullable()->default(null);
+            $table->dateTime('opening_date')->nullable()->default(null);
+            $table->string('waiting_banner')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Config_LP', function (Blueprint $table) {
+            $table->removeColumn('waiting_title');
+            $table->removeColumn('waiting_desription');
+            $table->removeColumn('opening_date');
+            $table->removeColumn('waiting_banner');
+        });
+    }
+}
