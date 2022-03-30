3b501be4d2a48b3cad07fe8e4c7c2703512686ac
Dalansi malek <dalansi.malek@gmail.com>
2022-03-30 15:27:13 +0100

    config des titres

---
+++ database/migrations/2022_03_30_113031_add_title_config-columns_to__config__congress.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTitleConfigColumnsToConfigCongress extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Config_Congress', function (Blueprint $table) {
+            $table->string("pack_title")->default("Choisir un pack");
+            $table->string("pack_title_en")->default("Choose a pack");
+            $table->string("access_title")->default("Veuillez sélectionner un (des) ticket(s)");
+            $table->string("access_title_en")->default("Choose Ticket");
+            $table->string("prise_charge_title")->default("Prise en charge");
+            $table->string("prise_charge_title_en")->default(" Supported by");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Config_Congress', function (Blueprint $table) {
+            $table->removeColumn("pack_title");
+            $table->removeColumn("pack_title_en");
+            $table->removeColumn("access_title");
+            $table->removeColumn("access_title_en");
+            $table->removeColumn("prise_charge_title");
+            $table->removeColumn("prise_charge_title_en");
+        });
+    }
+}
