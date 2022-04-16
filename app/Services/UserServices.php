185cbf41bcab44ee124785afcf0c661ebd1f0520
Akkeri <akkeriamani@gmail.com>
2022-04-16 06:44:26 +0200

    search

--- app/Services/UserServices.php
+++ app/Services/UserServices.php
@@ -1908,6 +1908,8 @@ class UserServices
 
             if ($search != "") {
                 $query->where(DB::raw('CONCAT(first_name," ",last_name)'), 'like', '%' . $search . '%');
+                $query->orWhereRaw('lower(chat_info) like (?)', ["%{$search}%"]);
+               
             }
         })
             ->with(['user_congresses'=> function ($query) use ($congressId){
