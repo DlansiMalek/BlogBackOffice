5a26488856540a3e08e987747899c37e57e277e3
Dalansi malek <dalansi.malek@gmail.com>
2022-05-19 16:10:39 +0100

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -762,23 +762,23 @@ class UserController extends Controller
                 $info = $this->userServices->getValueResponse($user->user_id, $formInputs[$i]->form_input_id);
                 if ($formInputs[$i]->form_input_type_id == 6 ||  $formInputs[$i]->form_input_type_id == 8) {
                     if(sizeof($info)>0) {
-                    foreach ($info as $inf) {
+                        foreach ($info as $inf) {
                         if (isset($inf['values']) && sizeof($inf['values']) > 0) {
                             $responses = $inf['values'][0]['val']['value'] . " " . $responses;
                         }
                     }
+                    }
+                    
                 } else {
                     if (isset($info) && sizeof($info) > 0) {
                         $responses = $info[0]['values'][0]['val']['value'] . " " . $responses;
                     }
                 }
-            
             } else {
                 $info = $this->userServices->getResponseFormInput($user->user_id, $formInputs[$i]->form_input_id);
                 $responses = $info[0]['response'] . " " . $responses;    
-            }
+            }  
         }
-           }
         }
 
         if ($responses) {
