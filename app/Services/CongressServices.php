3d8ce6a314bfad675638dd39ac36f5ff077df160
ABBES Yassine <abbes.yassine@outlook.fr>
2022-06-03 17:36:10 +0200

    add submission info

--- app/Services/CongressServices.php
+++ app/Services/CongressServices.php
@@ -23,7 +23,6 @@ use App\Models\Stand;
 use App\Models\Tracking;
 use App\Models\User;
 use App\Models\UserCongress;
-use App\Models\UserAccess;
 use DateTime;
 use Illuminate\Support\Facades\Config;
 use Tymon\JWTAuth\Facades\JWTAuth;
@@ -207,6 +206,9 @@ class CongressServices
                 "attestation",
                 "form_inputs.type",
                 "form_inputs.values",
+                'ConfigSubmission' => function ($query) use ($congressId) {
+                    $query->where('congress_id', '=', $congressId);
+                },
                 "form_inputs.question_reference"=> function ($query) {
                     $query->with(['reference', 
                     'response_reference'  => function ($q) {
@@ -1279,34 +1281,5 @@ class CongressServices
 
     public function countWillBePresentUserCongress($congress_id) {
         return UserCongress::where('congress_id', $congress_id)->where('will_be_present', 1)->count();
-    }
-
-    public function getAllUserCongressByCongressIdAndAccessId($congressId, $accessId)
-    {
-        return UserCongress::where('congress_id', '=', $congressId)
-        ->whereHas('user', function ($query) use ($accessId) {
-            $query->whereHas('user_access', function ($q) use ($accessId) {
-                $q->where('access_id', '=', $accessId)->where('isPresent', '=', 1);
-            });
-        })->get();
-    }
-
-    public function updateUserCongressDuration($congressId, $access)
-    {
-        $userCongresses = $this->getAllUserCongressByCongressIdAndAccessId($congressId, $access->access_id);
-        foreach($userCongresses as $userCongress) {
-            $userAccess  = $this->getUserAccess($userCongress->user_id, $access->access_id);
-            $userCongress->duration = $userCongress->duration + $userAccess->duration;
-            $userCongress->update();
-        }
-        $access->duration_set = 1;
-        $access->update();
-    }
-
-    public function getUserAccess($userId, $accessId)
-    {
-        return UserAccess::where('user_id', '=', $userId)
-        ->where('access_id', '=', $accessId)
-        ->first();
     }
 }
